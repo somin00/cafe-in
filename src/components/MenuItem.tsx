9aa9bdf8977b2e2d854ab5595064d09a0be95566
somin <smhs12@naver.com>
2023-07-23 01:13:08 +0900

    [🎨 style] 메뉴 리스트 마크업 및 스타일 적용 #1

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
-
+import styled from 'styled-components';
 function MenuItem() {
-	return <div>MenuItem</div>;
+	return (
+		<MenuItemWrapper>
+			<button type="button">
+				<img src="" alt="" />
+				<h3>아메리카노</h3>
+				<span>4,500원</span>
+			</button>
+		</MenuItemWrapper>
+	);
 }
 
 export default MenuItem;
+
+const MenuItemWrapper = styled.li`
+	background-color: ${({ theme }) => theme.textColor.white};
+	border: 1px solid #cccccc;
+	border-radius: 15px;
+	width: 250px;
+	height: 300px;
+	padding: 13px 16px 0 16px;
+
+	img {
+		width: 218px;
+		height: 204px;
+	}
+
+	h3 {
+		margin: 17px 0 7px 0;
+		font-size: ${({ theme }) => theme.fontSize['xl']};
+		font-weight: ${({ theme }) => theme.fontWeight.bold};
+	}
+
+	span {
+		font-size: ${({ theme }) => theme.fontSize.lg};
+		font-weight: ${({ theme }) => theme.fontWeight.semibold};
+	}
+`;
--- src/pages/admin/MenuManagement.tsx
+++ src/pages/admin/MenuManagement.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from '../../components/MenuManagement/Header';
 import CategoryList from '../../components/MenuManagement/CategoryList';
+import MenuList from '../../components/MenuManagement/MenuList';
 
 function MenuManagement() {
 	return (
-		<div>
+		<MenuManagementWrapper>
 			<Header />
 			<CategoryList />
-		</div>
+			<MenuList />
+		</MenuManagementWrapper>
 	);
 }
 
 export default MenuManagement;
+
+const MenuManagementWrapper = styled.div`
+	height: 100vh;
+	background-color: #f9f9f9;
+`;
