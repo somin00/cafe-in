6692782f6b6e85225f20304f076452ae04c34bde
jiyoon <vywns9978@gmail.com>
2023-07-24 16:30:57 +0900

    [🎨 style] 대기 관리 모달창 마크업 및 스타일 적용 #2

---
+++ src/components/waitingManagement/WaitingModal.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+function WaitingModal() {
+	return (
+		<WaitingModalWrapper>
+			<h1>대기를 취소하시겠습니까?</h1>
+			<ModalBtnWrapper>
+				<ModalBtn>예</ModalBtn>
+				<ModalBtn>아니오</ModalBtn>
+			</ModalBtnWrapper>
+		</WaitingModalWrapper>
+	);
+}
+
+export default WaitingModal;
+
+const WaitingModalWrapper = styled.div`
+	width: 622px;
+	height: 454px;
+	border-radius: 15px;
+	display: flex;
+	flex-flow: column nowrap;
+	justify-content: center;
+	align-items: center;
+
+	h1 {
+		font-size: ${({ theme }) => theme.fontSize['3xl']};
+		font-weight: ${({ theme }) => theme.fontWeight.semibold};
+		margin-bottom: 90px;
+		margin-top: 80px;
+	}
+`;
+
+const ModalBtnWrapper = styled.div`
+	width: 289px;
+	height: 65px;
+	display: flex;
+	justify-content: space-between;
+`;
+
+const ModalBtn = styled.button`
+	width: 110px;
+	height: 65px;
+	background-color: ${({ theme }) => theme.lightColor?.yellow.sub};
+	border-radius: 10px;
+	color: ${({ theme }) => theme.textColor.white};
+	font-size: ${({ theme }) => theme.fontSize['3xl']};
+	font-weight: ${({ theme }) => theme.fontWeight.semibold};
+`;
