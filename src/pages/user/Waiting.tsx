40e20204977e9fe1c2153aaffa7e63be3867f923
jiyoon <vywns9978@gmail.com>
2023-07-24 21:43:59 +0900

    [🎨 style] 대기 신청 페이지 다크모드 적용 #3

--- src/pages/user/Waiting.tsx
+++ src/pages/user/Waiting.tsx
@@ -10,21 +10,13 @@ function Waiting() {
 			<ApplicationBox>
 				<ApplicationHeaderText>대기를 원하시면 번호를 입력해주세요.</ApplicationHeaderText>
 				<NumCheckBox>
-					<img
-						src={process.env.PUBLIC_URL + '/assets/user/minusIcon_light.svg'}
-						alt="1 빼기 버튼"
-						aria-label="1 빼기"
-						role="button"
-						tabIndex={0}
-					/>
+					<MinusBtn>
+						<img alt="1 빼기 버튼" aria-label="1 빼기" />
+					</MinusBtn>
 					1
-					<img
-						src={process.env.PUBLIC_URL + '/assets/user/plusIcon_light.svg'}
-						alt="1 더하기 버튼"
-						aria-label="1 더하기"
-						role="button"
-						tabIndex={0}
-					/>
+					<PlusBtn>
+						<img alt="1 더하기 버튼" aria-label="1 더하기" />
+					</PlusBtn>
 				</NumCheckBox>
 				<InputBoxWrapper>
 					<InputBox type="text" placeholder="이름을 입력해주세요." required />
@@ -44,7 +36,8 @@ export default Waiting;
 const WaitingWrapper = styled.div`
 	width: 1194px;
 	height: 834px;
-	background-color: ${({ theme }) => theme.lightColor?.yellow.background};
+	background-color: ${({ theme }) =>
+		theme.lightColor ? theme.lightColor?.yellow.background : theme.darkColor?.background};
 	user-select: none;
 	display: flex;
 	align-items: center;
@@ -62,6 +55,7 @@ const WaitingHeaderText = styled.h1`
 	align-items: center;
 	font-weight: ${({ theme }) => theme.fontWeight.semibold};
 	font-size: ${({ theme }) => theme.fontSize['5xl']};
+	color: ${({ theme }) => (theme.lightColor ? theme.textColor.black : theme.textColor.white)};
 
 	p {
 		margin-top: 5px;
@@ -73,7 +67,7 @@ const ApplicationBox = styled.div`
 	height: 582px;
 	border-radius: 10px;
 	border: 1px solid ${({ theme }) => theme.textColor.lightgray};
-	background-color: ${({ theme }) => theme.textColor.white};
+	background-color: ${({ theme }) => (theme.lightColor ? theme.textColor.white : theme.darkColor?.background)};
 	display: flex;
 	flex-flow: column nowrap;
 	align-items: center;
@@ -85,6 +79,7 @@ const ApplicationHeaderText = styled.h2`
 	height: 32px;
 	margin-top: 20px;
 	font-size: ${({ theme }) => theme.fontSize['3xl']};
+	color: ${({ theme }) => (theme.lightColor ? theme.textColor.black : theme.textColor.white)};
 `;
 
 const NumCheckBox = styled.div`
@@ -96,9 +91,20 @@ const NumCheckBox = styled.div`
 	align-items: center;
 	padding-left: 16px;
 	padding-right: 16px;
+	color: ${({ theme }) => (theme.lightColor ? theme.textColor.black : theme.textColor.white)};
+`;
 
+const MinusBtn = styled.button`
 	img {
-		margin-bottom: 16px;
+		content: ${({ theme }) =>
+			theme.lightColor ? 'url(/assets/user/minusIcon_disable.svg)' : 'url(/assets/user/minusIcon_disable.svg)'};
+	}
+`;
+
+const PlusBtn = styled.button`
+	img {
+		content: ${({ theme }) =>
+			theme.lightColor ? 'url(/assets/user/plusIcon_light.svg)' : 'url(/assets/user/plusIcon_dark.svg)'};
 	}
 `;
 
@@ -135,7 +141,7 @@ const ApplicationBtn = styled.button`
 	border-radius: 10px;
 	color: ${({ theme }) => theme.textColor.white};
 
-	background-color: ${({ theme }) => theme.textColor.darkbrown};
+	background-color: ${({ theme }) => (theme.lightColor ? theme.textColor.darkbrown : theme.textColor.darkgray)};
 	font-size: ${({ theme }) => theme.fontSize['2xl']};
 	font-weight: ${({ theme }) => theme.fontWeight.semibold};
 `;
