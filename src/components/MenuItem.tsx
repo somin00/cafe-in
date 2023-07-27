3d45e981c0713ff658e645065686ed5bac66f3cd
somin <smhs12@naver.com>
2023-07-27 15:06:32 +0900

    [✨ feat] MenuItem 모달 사용자 모드 / 관리자 모드 구분 #14

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useState } from 'react';
 import { styled } from 'styled-components';
 import OptionMenu from './OptionMenu';
-import { darkTheme, defaultTheme } from '../style/theme';
+import { useRecoilValue } from 'recoil';
+import { selectedModeState } from '../state/Mode';
 
 function MenuItem() {
+	const mode = useRecoilValue(selectedModeState);
 	const [isOpenModal, setModalOpen] = useState<boolean>(false);
 
 	const onClickToggleModal = useCallback(() => {
@@ -11,49 +13,16 @@ function MenuItem() {
 	}, [isOpenModal]);
 
 	return (
-		<Layout>
-			<ItemContainer onClick={onClickToggleModal}>
-				{isOpenModal && <OptionMenu onClickToggleModal={onClickToggleModal}></OptionMenu>}
+		<MenuItemWrapper onClick={onClickToggleModal}>
+			{mode === 'user' && isOpenModal && <OptionMenu onClickToggleModal={onClickToggleModal}></OptionMenu>}
+			<button>
 				<img src="/assets/user/IceCoffee.svg" alt="Ice Coffee" />
 				<p className="menu-name"> 아메리카노 [Iced]</p>
 				<p className="menu-price">4,500원</p>
-			</ItemContainer>
-		</Layout>
+			</button>
+		</MenuItemWrapper>
 	);
 }
-const Layout = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr 1fr;
-	gap: 8px;
-	height: 830px;
-	margin: 30px 0;
-	overflow-y: auto;
-	overflow-x: hidden;
-	margin-right: 10px;
-	padding-left: 10px;
-	&::-webkit-scrollbar {
-		display: none;
-	}
-	/* Firefox */
-	scrollbar-width: none;
-`;
-
-const ItemContainer = styled.div`
-	background-color: ${({ theme }) =>
-		theme === defaultTheme ? defaultTheme.textColor.white : darkTheme.textColor.lightgray};
-	border: 1px solid ${({ theme }) => theme.textColor?.lightbrown};
-	border-radius: 15px;
-	padding: 13px 16px;
-	text-align: center;
-	height: fit-content;
-	.menu-name {
-		font-weight: ${({ theme }) => theme.fontWeight.bold};
-	}
-	.menu-price {
-		margin-top: 7px;
-		font-weight: ${({ theme }) => theme.fontWeight.semibold};
-	}
-`;
 
 export default MenuItem;
 
@@ -75,13 +44,13 @@ const MenuItemWrapper = styled.li`
 		height: 204px;
 	}
 
-	h3 {
+	.menu-name {
 		margin: 17px 0 7px 0;
 		font-size: ${({ theme }) => theme.fontSize['xl']};
 		font-weight: ${({ theme }) => theme.fontWeight.bold};
 	}
 
-	span {
+	p {
 		font-size: ${({ theme }) => theme.fontSize.lg};
 		font-weight: ${({ theme }) => theme.fontWeight.semibold};
 	}
