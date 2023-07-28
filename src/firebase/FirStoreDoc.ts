7637d5e8b61f98a26d31f379b0cbb0d2f0ef8118
Yujin <jyj15938@gmail.com>
2023-07-28 17:04:02 +0900

    [🔨 refactor] OptionMenu 컴포넌트를 리팩토링하여 Recoil 상태에서 옵션 데이터를 사용하도록 함

--- src/firebase/FirStoreDoc.ts
+++ src/firebase/FirStoreDoc.ts
@@ -1,9 +1,29 @@
 import { collection, getDocs } from 'firebase/firestore';
+import { selector } from 'recoil';
+import { Option } from '../state/OptinalState';
 import { db } from './firebaseConfig';
-export const FireStoreDoc = async () => {
+export const MenuItem = async () => {
 	const menuItemsRef = collection(db, 'menuItem');
 	const menuItemsSnapshot = await getDocs(menuItemsRef);
 	const menuItems = menuItemsSnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 
 	return menuItems;
 };
+
+//options 컬렉션 가져오고 배열로뿌려줌
+export const optionsState = selector({
+	key: 'optionsState',
+	get: async () => {
+		const optionsRef = collection(db, 'options');
+		const optionsSnapshot = await getDocs(optionsRef);
+		const optionsData = optionsSnapshot.docs.reduce(
+			(acc, doc) => {
+				acc[doc.id] = doc.data().choices;
+				return acc;
+			},
+			{} as Record<string, Option[]>,
+		);
+
+		return optionsData;
+	},
+});
--- src/state/OptinalState.ts
+++ src/state/OptinalState.ts
@@ -6,7 +6,7 @@ export interface Option {
 	price: number;
 }
 
-export const selectedOptionsState = atom<Option[]>({
+export const selectedOptionsState = atom<Record<string, Option[]>>({
 	key: 'selectedOptionsState',
-	default: [],
+	default: {},
 });
