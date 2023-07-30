978d97b695533e41c573e393c16f3434cbfe7716
Yujin <jyj15938@gmail.com>
2023-07-31 03:37:05 +0900

    [✨feat] seletedItem DB 연결 수량 추가

--- src/firebase/FirStoreDoc.ts
+++ src/firebase/FirStoreDoc.ts
@@ -1,17 +1,18 @@
-import { collection, getDocs } from 'firebase/firestore';
 import { atom } from 'recoil';
-import { db } from './firebaseConfig';
 import { Option } from '../state/OptinalState';
-
-export const FireStoreDoc = async () => {
-	const menuItemsRef = collection(db, 'menuItem');
-	const menuItemsSnapshot = await getDocs(menuItemsRef);
-	const menuItems = menuItemsSnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-
-	return menuItems;
-};
+import { collection, getDocs } from 'firebase/firestore';
 
 export const selectedOptionsState = atom<Option[]>({
 	key: 'selectedOptionsState',
 	default: [],
 });
+export const fetchSelectedItems = async (db: any) => {
+	const selectedItemRef = collection(db, 'selectedItem');
+	const selectedItemSnapshot = await getDocs(selectedItemRef);
+	const selectedItems = selectedItemSnapshot.docs.map((doc: { data: () => any; id: any }) => ({
+		...doc.data(),
+		id: doc.id,
+	}));
+
+	return selectedItems;
+};
