bca083199de187ca85be6271e9ee315621eb49b3
Yujin <jyj15938@gmail.com>
2023-07-31 23:21:20 +0900

    [🔨 refactor] 충돌 해결

--- src/firebase/FirStoreDoc.ts
+++ src/firebase/FirStoreDoc.ts
@@ -1,10 +1,6 @@
 import { atom } from 'recoil';
-<<<<<<< HEAD
 import { Option } from '../state/OptinalState';
 import { Item } from '../state/Category';
-=======
-import { Option, selectedItem } from '../state/OptinalState';
->>>>>>> c08ba48d68d30c994c0b606f18f127e324718c4f
 
 export const selectedOptionsState = atom<Option[]>({
 	key: 'selectedOptionsState',
@@ -12,7 +8,6 @@ export const selectedOptionsState = atom<Option[]>({
 });
 
 //사용자가 선택한 메뉴 아이템
-<<<<<<< HEAD
 export const selectedItemsState = atom({
 	key: 'selectedItemsState',
 	default: {} as Item,
@@ -22,20 +17,3 @@ export const menuItemState = atom<Item[]>({
 	key: 'menuItemState',
 	default: [],
 });
-=======
-export const selectedItemsState = atom<selectedItem[]>({
-	key: 'selectedItemsState',
-	default: [],
-});
-
-//현재 메뉴 아이템
-export const menuItemState = atom({
-	key: 'menuItemState',
-	default: {
-		id: 0,
-		name: '',
-		price: 0,
-		category: '',
-	},
-});
->>>>>>> c08ba48d68d30c994c0b606f18f127e324718c4f
