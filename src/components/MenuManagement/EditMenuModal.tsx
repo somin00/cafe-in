ef0bf8495142a6db771f255bf56294b8605cf706
somin <smhs12@naver.com>
2023-08-04 18:25:47 +0900

    [✨feat] 메뉴 수정 기능 구현 #31

--- src/components/MenuManagement/EditMenuModal.tsx
+++ src/components/MenuManagement/EditMenuModal.tsx
@@ -1,22 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ModalInput from './ModalInput';
+import { MenuType } from '../../types/menuMangementType';
+import { db } from '../../firebase/firebaseConfig';
+import { getDocs, query, where, updateDoc, collection } from 'firebase/firestore';
+import { getStorage, ref as storageRef, getDownloadURL, uploadBytes } from 'firebase/storage';
 
-function EditMenuModal() {
+interface EditModalPropType {
+	menu: MenuType;
+	onCloseModal: () => void;
+}
+function EditMenuModal({ menu, onCloseModal }: EditModalPropType) {
+	const [menuInfo, setMenuInfo] = useState<MenuType>(menu);
+	const [file, setFile] = useState<File>();
+
+	const menuItemRef = collection(db, 'menuItem');
+	const storage = getStorage();
+
+	const storeImg = async () => {
+		if (!file) return;
+		const imageRef = storageRef(storage, `images/${menuInfo.imageName}`);
+		const snapshot = await uploadBytes(imageRef, file);
+		const url = await getDownloadURL(snapshot.ref);
+		return url;
+	};
+
+	const handleEditMenu = async () => {
+		const imageUrl = await storeImg();
+		const menuData = await getDocs(query(menuItemRef, where('id', '==', menuInfo.id)));
+		if (menuData.docs.length !== 0) {
+			await updateDoc(menuData.docs[0].ref, {
+				id: menuInfo.id,
+				name: menuInfo.name,
+				price: menuInfo.price,
+				category: menuInfo.category,
+				imageName: menuInfo.imageName,
+				imageUrl: imageUrl ? imageUrl : menuInfo.imageUrl,
+				soldout: menuInfo.soldout,
+			});
+		}
+		onCloseModal();
+	};
+
+	const handleDeleteEdit = () => {
+		setMenuInfo(menu);
+		onCloseModal();
+	};
+
 	return (
 		<EditModalWrapper>
-			{/* <ModalInput /> */}
-			<div>
-				<Button type="button">수정</Button>
-				<Button type="button">삭제</Button>
-				<Button type="button">취소</Button>
-			</div>
+			<EditModalContent>
+				<ModalInput menuInfo={menu} setMenuState={setMenuInfo} setFile={setFile} />
+				<div>
+					<Button type="button" onClick={handleEditMenu}>
+						수정
+					</Button>
+					<Button type="button">삭제</Button>
+					<Button type="button" onClick={handleDeleteEdit}>
+						취소
+					</Button>
+				</div>
+			</EditModalContent>
 		</EditModalWrapper>
 	);
 }
 
 export default EditMenuModal;
+
 const EditModalWrapper = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.2);
+	position: fixed;
+	left: 0;
+	top: 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const EditModalContent = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -34,8 +97,7 @@ const Button = styled.button`
 	font-size: ${({ theme }) => theme.fontSize['3xl']};
 	font-weight: ${({ theme }) => theme.fontWeight.regular};
 	color: ${({ theme }) => theme.textColor.white};
-	/* 색상 코드 추가되면 수정 */
-	background-color: ${({ theme }) => (theme.lightColor ? theme.lightColor?.yellow.sub : '#068FFF')};
+	background-color: ${({ theme }) => (theme.lightColor ? theme.lightColor?.yellow.sub : theme.darkColor?.main)};
 
 	&:not(:last-child) {
 		margin-right: 24px;
