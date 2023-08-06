5d571f982868e283f1aa2187d65a6da2543ef2f0
jiyoon <vywns9978@gmail.com>
2023-08-06 14:57:52 +0900

    [✨ feat] 시간이 지나면 자동으로 페이지 이동 추가 #37

--- src/pages/user/WaitingCheck.tsx
+++ src/pages/user/WaitingCheck.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
@@ -7,6 +8,24 @@ function WaitingCheck() {
 
 	//? 대기 신청 페이지에서 넘겨 받은 대기 번호
 	const { userWaitingNum } = location.state;
+
+	const goToHome = () => {
+		navigate('/home');
+	};
+
+	const [remainingTime, setRemainingTime] = useState<number>(20);
+
+	useEffect(() => {
+		const timeoutId = setTimeout(() => {
+			setRemainingTime((prevTime) => prevTime - 1);
+		}, 1000);
+
+		if (remainingTime === 0) {
+			goToHome();
+		}
+
+		return () => clearTimeout(timeoutId);
+	}, [remainingTime]);
 
 	return (
 		<WaitingCheckWrapper>
@@ -18,7 +37,8 @@ function WaitingCheck() {
 				<WaitingNumText>{userWaitingNum}번</WaitingNumText>
 				<RightFireworks />
 			</WaitingNumWrapper>
-			<HomeBtn onClick={() => navigate('/home')}>
+			<RemainingTimeText>{remainingTime}초 후에 홈 화면으로 이동됩니다.</RemainingTimeText>
+			<HomeBtn onClick={goToHome}>
 				홈화면으로 <p>이동하기</p>
 			</HomeBtn>
 		</WaitingCheckWrapper>
@@ -48,6 +68,7 @@ const WaitingCheckHeaderText = styled.h1`
 	flex-flow: column nowrap;
 	justify-content: center;
 	align-items: center;
+	padding-top: 10px;
 
 	p {
 		margin-top: 10px;
@@ -89,6 +110,12 @@ const WaitingNumText = styled.div`
 	align-items: center;
 	padding-bottom: 7px;
 	border-bottom: 1px solid ${({ theme }) => theme.textColor.white};
+`;
+
+const RemainingTimeText = styled.p`
+	font-size: ${({ theme }) => theme.fontSize['3xl']};
+	font-weight: ${({ theme }) => theme.fontWeight.semibold};
+	color: ${({ theme }) => (theme.lightColor ? theme.textColor.black : theme.textColor.white)};
 `;
 
 const HomeBtn = styled.button`
