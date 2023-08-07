4790046860ef03f093704995657e4e0bc0cf7394
jiyoon <vywns9978@gmail.com>
2023-08-07 16:22:19 +0900

    [🔥 remove] 불필요한 코드 삭제 #43

--- src/pages/admin/ThemeManagement.tsx
+++ src/pages/admin/ThemeManagement.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { styled } from 'styled-components';
 import ManagementHeader from '../../components/adminMode/ManagementHeader';
-import { url } from 'inspector';
 
 function ThemeManagement() {
 	return (
