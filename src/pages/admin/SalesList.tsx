e1027a0c91ff2507e3bbefbeb4db7afe141276e7
jiyoon <vywns9978@gmail.com>
2023-08-15 00:46:00 +0900

    [✨ feat] 특정 날짜의 주문 건 수, 총 매출 액 및 특정 달의 매출 렌더링 #75

--- src/pages/admin/SalesList.tsx
+++ src/pages/admin/SalesList.tsx
@@ -1,24 +1,106 @@
+import { useEffect, useState } from 'react';
 import withAuth from '../../components/adminMode/WithAuth';
 import { styled } from 'styled-components';
 import ManagementHeader from '../../components/adminMode/ManagementHeader';
+import { db } from '../../firebase/firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
+
+interface Order {
+	id: number;
+	list: {
+		menu: string;
+		quantity: number;
+		options: string;
+		isComplete: boolean;
+	}[];
+	totalPrice: number;
+	takeout: boolean;
+	progress: '진행중' | '완료주문';
+}
+
+interface DaySalesData {
+	date: string;
+	totalPriceSum: number;
+	orderCount: number;
+}
 
 function SalesList() {
+	const [salesList, setSalesList] = useState<Order[]>([]);
+	const [daySalesData, setDaySalesData] = useState<DaySalesData[]>([]);
+
+	useEffect(() => {
+		const getSales = async () => {
+			const orderListCollection = collection(db, 'testOrderList');
+			const salesDocs = await getDocs(orderListCollection);
+			const salesDataList: Order[] = [];
+			salesDocs.forEach((doc) => {
+				salesDataList.push({ ...(doc.data() as Order) });
+			});
+			setSalesList(salesDataList);
+		};
+
+		getSales();
+	}, []);
+
+	useEffect(() => {
+		const salesByDate: { [date: string]: { salesData: Order[]; totalPriceSum: number } } = {};
+
+		salesList.forEach((order) => {
+			const date = new Date(order.id).toISOString().split('T')[0];
+
+			if (!salesByDate[date]) {
+				salesByDate[date] = { salesData: [], totalPriceSum: 0 };
+			}
+
+			salesByDate[date].salesData.push(order);
+			salesByDate[date].totalPriceSum += order.totalPrice;
+		});
+
+		const daySalesList: DaySalesData[] = [];
+
+		for (const date in salesByDate) {
+			const { salesData, totalPriceSum } = salesByDate[date];
+			daySalesList.push({ date, totalPriceSum, orderCount: salesData.length });
+		}
+
+		setDaySalesData(daySalesList);
+	}, [salesList]);
+
+	const sampleData = daySalesData.filter((v) => v.date === '2023-08-14');
+
+	// 월 별 데이터 가져오기
+	const currentDate = new Date();
+	const targetYear = currentDate.getFullYear();
+	const targetMonth = currentDate.getMonth() + 1;
+
+	const filterData = daySalesData.filter((data) => {
+		const stringDate = data.date.split('-');
+		const year = parseInt(stringDate[0]);
+		const month = parseInt(stringDate[1]);
+
+		return year === targetYear && month === targetMonth;
+	});
+
+	// console.log(filterData);
+
 	return (
 		<SalesListWrapper>
 			<ManagementHeader headerText="매출 내역 조회" />
 			<SalesContent>
 				<DailySalesWrapper>
 					<p>
-						<button aria-label="전 날 매출 보기">{'<'}</button> 08/13 매출{' '}
+						<button aria-label="전 날 매출 보기">{'<'}</button>
+						{sampleData.length > 0 ? <span> {sampleData[0].date} </span> : <span>로딩중 </span>}
 						<button aria-label="다음 날 매출 보기">{'>'}</button>
 					</p>
 					<SalesBoxWrapper>
 						<SalesBox className="totalOrder">
 							<p>주문 건 수</p>
-							<p>123건</p>
+							{sampleData.length > 0 ? <p> {sampleData[0].orderCount}건</p> : <p>0 건</p>}
 						</SalesBox>
 						<SalesBox>
-							<p>총 매출 액</p> <p> 12,345원</p>
+							<p>총 매출 액</p>
+							{sampleData.length > 0 ? <p> {sampleData[0].totalPriceSum}원</p> : <p>0원</p>}
 						</SalesBox>
 					</SalesBoxWrapper>
 				</DailySalesWrapper>
@@ -29,7 +111,7 @@ function SalesList() {
 					</p>
 					<SalesBoxWrapper>
 						<SalesBox>
-							<p>8월 매출</p> <p>12,345원</p>
+							<p>8월 매출</p> {filterData.length > 0 ? <p> {filterData.length}건</p> : <p>0 건</p>}
 						</SalesBox>
 					</SalesBoxWrapper>
 				</MonthlySalesWrapper>
