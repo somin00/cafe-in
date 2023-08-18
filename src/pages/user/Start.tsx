5a1f0648d2ae0894a3117c1cf766bae82727b437
jiyoon <vywns9978@gmail.com>
2023-08-18 23:48:57 +0900

    [🎨 style] 시작 페이지 뒤로가기 아이콘 다크모드 설정 #110

--- src/pages/user/Start.tsx
+++ src/pages/user/Start.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { styled } from 'styled-components';
+import { styled, useTheme } from 'styled-components';
 import { isWaitingAvailableState } from '../../state/WaitingState';
 import { takeOutState } from '../../state/TakeOut';
 
@@ -9,6 +9,7 @@ function Start() {
 	const navigate = useNavigate();
 	const isWaitingAvailable = useRecoilValue<boolean>(isWaitingAvailableState);
 	const setTakeOut = useSetRecoilState(takeOutState);
+	const theme = useTheme();
 	return (
 		<StartWrapper>
 			<BackIconWrapper>
@@ -17,7 +18,13 @@ function Start() {
 						navigate('/');
 					}}
 				>
-					<img src={process.env.PUBLIC_URL + '/assets/admin/back_light.svg'} alt="뒤로가기 버튼" />
+					<img
+						src={
+							process.env.PUBLIC_URL +
+							(theme.lightColor ? '/assets/admin/back_light.svg' : '/assets/admin/back_dark.svg')
+						}
+						alt="뒤로가기 버튼"
+					/>
 				</BackIcon>
 			</BackIconWrapper>
 			<TakeOutBoxWrapper>
